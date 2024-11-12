684bf52603b3a0e59e851a6b325e19484eb0051b
Hugo Daclon <hugo.daclon@gmail.com>
2024-11-12 10:15:29 +0100

    feat: answer part 2

--- part2-wordcount-hamlet-pyspark-2.py
+++ part2-wordcount-hamlet-pyspark-2.py
@@ -1,25 +1,21 @@
-from pyspark.sql import SparkSession
-from pyspark.sql import functions
-from pyspark.sql.functions import split
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import col
-from pyspark.sql.functions import lower
-
-import re
 import time
-import sys
 
-spark = SparkSession.builder.master('local[*]').appName('BigDataTP').getOrCreate()
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, explode, lower, split
+
+spark = SparkSession.builder.master(
+    'local[*]').appName('BigDataTP').getOrCreate()
 sc = spark.sparkContext
 sc.setLogLevel("WARN")
 
-print ("Start")
+print("Start")
 start_time = time.time()
 
 text_file = spark.read.text('dataset/wordcount/hamlet.txt')
-words = text_file.select(explode(split(text_file.value, "\s+")).alias("word"))
+words = text_file.select(explode(split(text_file.value, "\\s+")).alias("word"))
 words = words.filter("word is not NULL")
 words = words.filter("word != ''")
+words = words.filter("word like 'm%' or word like 'M%'")
 words = words.withColumn("word", lower(col("word")))
 
 word_counts = words.groupBy("word").count()
@@ -28,5 +24,5 @@ word_counts = words.groupBy("word").count()
 
 word_counts.orderBy(col("count").desc()).show(10)
 
-print ("End")
+print("End")
 print("--- %s seconds ---" % (time.time() - start_time))
--- part2-wordcount-hamlet-pyspark.py
+++ part2-wordcount-hamlet-pyspark.py
@@ -1,33 +1,39 @@
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as f
-
 import re
+import sys
 import time
 
-pattern = re.compile("^[a-z]+$")
+from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.master('local[*]').appName('BigDataTP').getOrCreate()
+debug = "--debug" in sys.argv or "-d" in sys.argv
+cache = "--cache" in sys.argv or "-c" in sys.argv
+
+pattern = re.compile("^[mM][a-z]+$")
+
+spark = SparkSession.builder.master(
+    'local[*]').appName('BigDataTP').getOrCreate()
 sc = spark.sparkContext
 sc.setLogLevel("WARN")
 
-print ("Start")
+print("Start")
 start_time = time.time()
 
 text_file = sc.textFile("dataset/wordcount/hamlet.txt")
-# print(text_file.collect())
+print(text_file.collect()) if debug else None
 
 words = text_file.flatMap(lambda line: line.split(" "))
-# print(words.collect())
+print(words.collect()) if debug else None
 
-words_filtered = words.map(lambda x: x.lower()).filter(lambda x: pattern.match(x))
-# print(words_filtered.collect())
+words_filtered = words.map(lambda x: x.lower()).filter(
+    lambda x: pattern.match(x))
+print(words_filtered.collect()) if debug else None
 
-wordCounts = words_filtered.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
-# print(wordCounts.collect())
-# wordCounts.saveAsTextFile("output/wordcount/")
+wordCounts = words_filtered.map(lambda word: (
+    word, 1)).reduceByKey(lambda a, b: a + b)
+print(wordCounts.collect()) if debug else None
+wordCounts.saveAsTextFile("output/wordcount/") if cache else None
 
-wordCounts_sorted = wordCounts.sortBy(lambda a:a[1],ascending=False).take(10)
+wordCounts_sorted = wordCounts.sortBy(lambda a: a[1], ascending=False).take(10)
 print(wordCounts_sorted)
 
-print ("End")
+print("End")
 print("--- %s seconds ---" % (time.time() - start_time))
